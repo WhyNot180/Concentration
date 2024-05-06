4682b5c40d0ee036d647fe1eb7ba485333702145
Bence Szucs <bence.szucs200625@gmail.com>
2024-05-06 18:54:09 -0400

    changes layout to use material components and adds image list of cards

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,19 @@
-module Main exposing (main,cartesianProduct)
+module Main exposing (cardRankToImageName, cardSuitToImageName, cartesianProduct, main)
 
-import Html exposing (..)
 import Browser
-import Html.Attributes exposing (..)
-import Html.Events exposing(onClick)
+import Html exposing (Html)
+import Html.Attributes exposing (style)
+import Html.Events exposing (onClick)
+import Material.Button as Button
+import Material.ImageList as ImageList
+import Material.ImageList.Item as ImageListItem
+import Material.LayoutGrid as LayoutGrid
+import Material.Typography as Typography
 import Random exposing (generate)
 import Random.List exposing (shuffle)
 
-type CardRank 
+
+type CardRank
     = Joker
     | Ace
     | King
@@ -23,65 +29,151 @@ type CardRank
     | Three
     | Two
 
+
 type CardSuit
     = Hearts
     | Spades
     | Diamonds
     | Clubs
 
-type alias Model =
-    {cards : List (CardRank,CardSuit)}
 
-initialModel : () -> (Model, Cmd Msg)
+cardRankToImageName : CardRank -> String
+cardRankToImageName rank =
+    case rank of
+        Joker ->
+            "Jo"
+
+        Ace ->
+            "A"
+
+        King ->
+            "K"
+
+        Queen ->
+            "Q"
+
+        Jack ->
+            "J"
+
+        Ten ->
+            "10"
+
+        Nine ->
+            "9"
+
+        Eight ->
+            "8"
+
+        Seven ->
+            "7"
+
+        Six ->
+            "6"
+
+        Five ->
+            "5"
+
+        Four ->
+            "4"
+
+        Three ->
+            "3"
+
+        Two ->
+            "2"
+
+
+cardSuitToImageName : CardSuit -> String
+cardSuitToImageName suit =
+    case suit of
+        Hearts ->
+            "H"
+
+        Spades ->
+            "S"
+
+        Diamonds ->
+            "D"
+
+        Clubs ->
+            "C"
+
+
+type alias Model =
+    { cards : List ( CardRank, CardSuit ) }
+
+
+initialModel : () -> ( Model, Cmd Msg )
 initialModel _ =
-    ( Model [], Cmd.none)
+    ( { cards =
+            cartesianProduct
+                [ Joker, Ace, King, Queen, Jack, Ten, Nine, Eight, Seven, Six, Five, Four, Three, Two ]
+                [ Hearts, Spades, Diamonds, Clubs ]
+      }
+    , Cmd.none
+    )
+
 
 type Msg
-    = ListCards (List (CardRank,CardSuit))
+    = ListCards (List ( CardRank, CardSuit ))
     | GameStart
+
 
 view : Model -> Html Msg
 view model =
-    div [ class "jumbotron" ]
-        [ h1 [] [ text "Welcome to Dunder Mifflin!" ]
-        , p []
-            [ text "Dunder Mifflin Inc. (stock symbol "
-            , strong [] [ text "DMI" ]
-            , text <|
-                """ 
-                ) is a micro-cap regional paper and office 
-                supply distributor with an emphasis on servicing 
-                small-business clients.
-                """
-            ]
-        , div [] [button [onClick GameStart] [text "Start"],
-                    div [] [text (Debug.toString model)]
+    LayoutGrid.layoutGrid [ Typography.typography ]
+        [ LayoutGrid.inner []
+            [ LayoutGrid.cell [ LayoutGrid.span1Phone, LayoutGrid.alignTop ] [ Html.h3 [ Typography.headline3 ] [ Html.text "Turns: " ] ]
+            , LayoutGrid.cell [ LayoutGrid.span2Phone, LayoutGrid.alignTop ] [ Html.h2 [ Typography.headline2 ] [ Html.text "00:00:00" ] ]
+            , LayoutGrid.cell [ LayoutGrid.span1Phone, LayoutGrid.alignTop ] [ Button.raised (Button.config |> Button.setOnClick GameStart) "Start" ]
+            , LayoutGrid.cell [ LayoutGrid.span12Desktop, LayoutGrid.span4Phone ]
+                [ ImageList.imageList (ImageList.config |> ImageList.setMasonry True)
+                    (List.map
+                        (\( x, y ) ->
+                            ImageListItem.imageListItem
+                                (ImageListItem.config
+                                    |> ImageListItem.setAttributes [ style "width" "calc(100% / 5 - 4px)", style "margin" "2px", style "height" "auto" ]
+                                )
+                            <|
+                                "../Images/Cards/"
+                                    ++ cardRankToImageName x
+                                    ++ cardSuitToImageName y
+                                    ++ ".png"
+                        )
+                        model.cards
+                    )
                 ]
+            ]
         ]
 
-update : Msg -> Model -> (Model, Cmd Msg)
-update msg model = 
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
     case msg of
         GameStart ->
-            (model, cartesianProduct 
-                [Joker, Ace, King, Queen, Jack, Ten, Nine, Eight, Seven, Six, Five, Four, Three, Two] 
-                [Hearts, Spades, Diamonds, Clubs] 
-            |> shuffle
-            |> generate ListCards)
-        
+            ( model
+            , cartesianProduct
+                [ Joker, Ace, King, Queen, Jack, Ten, Nine, Eight, Seven, Six, Five, Four, Three, Two ]
+                [ Hearts, Spades, Diamonds, Clubs ]
+                |> shuffle
+                |> generate ListCards
+            )
+
         ListCards cardList ->
-            (Model cardList, Cmd.none)
+            ( Model cardList, Cmd.none )
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
 
-cartesianProduct : List a -> List b -> List (a,b)
+
+cartesianProduct : List a -> List b -> List ( a, b )
 cartesianProduct xs ys =
-  List.concatMap
-    ( \x -> List.map ( \y -> (x, y) ) ys )
-    xs
-    
+    List.concatMap
+        (\x -> List.map (\y -> ( x, y )) ys)
+        xs
+
 
 main =
-    Browser.element { init = initialModel, subscriptions = subscriptions, update = update, view = view}
+    Browser.element { init = initialModel, subscriptions = subscriptions, update = update, view = view }
