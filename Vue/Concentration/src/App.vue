1af866f87bcb7205776e18ed32cdef4274ef5b9a
Bence Szucs <bence.szucs200625@gmail.com>
2024-05-10 18:31:39 -0400

    adds basic html components

---
+++ Vue/Concentration/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('Hello World!')
+
+const turns = ref(0)
+
+const time = ref(0)
+
+const cards = ref([{ rank: 1, suit: 0}])
+
+function start() {
+  turns.value++
+}
+
+</script>
+
+<template>
+  <h3>turns: {{ turns }}</h3>
+  <h2>{{ time }}</h2>
+  <button @click="start">Start</button>
+</template>
--- Vue/Concentration/src/main.js
+++ Vue/Concentration/src/main.js
@@ -3,4 +3,4 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-const app = createApp(App)
+createApp(App).mount('#app')
