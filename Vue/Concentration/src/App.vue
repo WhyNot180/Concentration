25f1ea2d4e3187d774c2280b82215e0ba9e77bb6
Bence Szucs <bence.szucs200625@gmail.com>
2024-05-10 18:39:00 -0400

    adds cartesian product method

--- Vue/Concentration/src/App.vue
+++ Vue/Concentration/src/App.vue
@@ -7,10 +7,18 @@ const turns = ref(0)
 
 const time = ref(0)
 
-const cards = ref([{ rank: 1, suit: 0}])
+const suit = [1,2,3,4]
+const rank = [0,1,2,3,4,5,6,7,8,9,10,11,12,13]
+
+const cards = ref(cartesianProduct(suit,rank))
+
 
 function start() {
   turns.value++
+}
+
+function cartesianProduct(...a) {
+  return a.reduce((a, b) => a.flatMap(d => b.map(e => [d, e].flat())))
 }
 
 </script>
